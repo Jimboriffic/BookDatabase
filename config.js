e90c43b744178e7335fa07e4f619229a11076d05
Jimboriffic <james.sliwa@gmail.com>
2023-09-14 18:55:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const PORT = 5555;
 
+//create your own mongoDB database
 export const mongoDBURL = 
 'mongodb+srv://root:<password>@bookstore-mern.wd0sd9g.mongodb.net/books-collection?retryWrites=true&w=majority';
